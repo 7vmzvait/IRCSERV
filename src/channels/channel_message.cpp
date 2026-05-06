93c924a6abf86e80bb62d98dfe392c98bb955c5f
Hamza Aitaabella <haitaabe@e2r1p3.1337.ma>
2026-05-06 20:24:48 +0100

    Its not the last version hh

--- src/channels/channel_message.cpp
+++ src/channels/channel_message.cpp
@@ -14,88 +14,101 @@
 #include "../../include/utls.hpp"
 #include "../../include/server.hpp"
 
-void managerchannel::handlePrivmsg(const std::string &input, client &c) 
+void managerchannel::handlePrivmsg(const std::string &input, client &c)
 {
     if (!c.regestred)
     {
-        std::string err = ":ircserv 451 * :You have not registered\r\n";
+        std::string err = ":ircserv 451 " + (c.nickname.empty() ? "*" : c.nickname) + " :You have not registered\r\n";
         send(c.fd, err.c_str(), err.size(), 0);
         return;
     }
+
     std::stringstream ss(input);
-    std::string command, target;
-    ss >> command >> target;
-
-    // 1. Locate the message start (handling the colon correctly)
-    size_t pos = input.find(':', input.find(target));
-    std::string message;
-    if (pos != std::string::npos) {
-        message = input.substr(pos); 
-    } else {
-        // Fallback for non-standard clients
-        ss >> message; 
+    std::string command, allTargets;
+    ss >> command >> allTargets;
+    size_t colon_pos = input.find(':');
+    if (colon_pos == std::string::npos || allTargets.empty())
+    {
+        std::string err = ":ircserv 412 " + c.nickname + " :No text to send\r\n";
+        send(c.fd, err.c_str(), err.size(), 0);
+        return;
     }
+    std::string message_content = input.substr(colon_pos);
 
-    if (target.empty() || message.empty()) return;
+    std::vector<std::string> targets = splitByComma(allTargets);
 
-    // 2. The Protocol Shield (Strict 512-byte IRC limit)
-    std::string prefix = ":" + c.nickname + " PRIVMSG " + target + " ";
-    std::string full_msg = prefix + message;
-    
-    if (full_msg.size() > 510) {
-        full_msg = full_msg.substr(0, 510);
-    }
-    full_msg += "\r\n";
-    /// PRIVMASG NICK: hello 
-    // 3. Routing & Security Gates
-    if (target[0] == '#') {
-        // --- CHANNEL BROADCAST ---
-        if (channels.find(target) != channels.end()) {
-            Channel *room = channels[target];
+    for (size_t t = 0; t < targets.size(); ++t)
+    {
+        std::string target = targets[t];
 
-            // SECURITY CHECK: Is the sender actually IN this channel?
-            bool is_member = false;
-            for (size_t i = 0; i < room->members.size(); i++) {
-                if (room->members[i] == c.fd) {
-                    is_member = true;
+        std::string full_msg = ":" + c.nickname + " PRIVMSG " + target + " " + message_content + "\r\n";
+        if (full_msg.size() > 512)
+            full_msg = full_msg.substr(0, 510) + "\r\n";
+
+        if (target[0] == '#')
+        {
+            std::map<std::string, Channel *>::iterator it_chan = channels.find(target);
+
+            if (it_chan != channels.end())
+            {
+                Channel *room = it_chan->second;
+
+                bool is_member = false;
+                for (size_t i = 0; i < room->members.size(); i++)
+                {
+                    if (room->members[i] == c.fd)
+                    {
+                        is_member = true;
+                        break;
+                    }
+                }
+
+                if (!is_member)
+                {
+                    std::string err = ":ircserv 404 " + c.nickname + " " + target + " :Cannot send to channel\r\n";
+                    send(c.fd, err.c_str(), err.size(), 0);
+                    continue;
+                }
+
+                for (size_t i = 0; i < room->members.size(); i++)
+                {
+                    if (room->members[i] != c.fd)
+                    {
+                        send(room->members[i], full_msg.c_str(), full_msg.size(), 0);
+                    }
+                }
+            }
+            else
+            {
+
+                std::string err = ":ircserv 401 " + c.nickname + " " + target + " :No such nick/channel\r\n";
+                send(c.fd, err.c_str(), err.size(), 0);
+            }
+        }
+        else
+        {
+            bool found_and_active = false;
+
+            for (std::map<int, client>::iterator it_cli = _clients.begin(); it_cli != _clients.end(); ++it_cli)
+            {
+                if (it_cli->second.nickname == target)
+                {
+
+                    if (it_cli->second.regestred == true)
+                    {
+                        send(it_cli->first, full_msg.c_str(), full_msg.size(), 0);
+                        found_and_active = true;
+                    }
+
                     break;
                 }
             }
 
-            if (!is_member) {
-                // Error 404: Cannot send to channel (they were likely KICKED or PARTED)
-                std::string err = ":ircserv 404 " + c.nickname + " " + target + " :Cannot send to channel\r\n";
+            if (!found_and_active)
+            {
+                std::string err = ":ircserv 401 " + c.nickname + " " + target + " :No such nick/channel\r\n";
                 send(c.fd, err.c_str(), err.size(), 0);
-                return;
             }
-
-            // Broadcast to everyone EXCEPT the sender
-            for (size_t i = 0; i < room->members.size(); i++) {
-                if (room->members[i] != c.fd) {
-                    send(room->members[i], full_msg.c_str(), full_msg.size(), 0);
-                }
-            }
-        } else {
-            // Error 401: No such channel
-            std::string err = ":ircserv 401 " + c.nickname + " " + target + " :No such nick/channel\r\n";
-            send(c.fd, err.c_str(), err.size(), 0);
-        }
-    } 
-    else {
-        // --- PRIVATE MESSAGE TO USER ---
-        bool found = false;
-        for (std::map<int, client>::iterator it = _clients.begin(); it != _clients.end(); ++it) {
-            if (it->second.nickname == target) {
-                send(it->first, full_msg.c_str(), full_msg.size(), 0);
-                found = true;
-                break;
-            }
-        }
-        
-        if (!found) {
-            // Error 401: User not found (they likely QUIT)
-            std::string err = ":ircserv 401 " + c.nickname + " " + target + " :No such nick/channel\r\n";
-            send(c.fd, err.c_str(), err.size(), 0);
         }
     }
 }
